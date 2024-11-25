bcc89b2339005e870196db9995c245d795819975
KangMe17 <vukhangpln123@gmail.com>
2024-11-25 16:04:22 +0700

    khang

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM Users u WHERE u. username = :username")
+	public User getUserByUsername(@Param("username") String username);
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByUsernameOrEmail(String username, String email);
+
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+}
---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Product;
+
+public interface ProductService {
+	void delete(Long id);
+
+	Product get(Long id);
+
+	Product save(Product product);
+
+	List<Product> listAll();
+}
